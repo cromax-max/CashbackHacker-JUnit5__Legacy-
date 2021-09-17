3d399e93ee3de49a46bd92d70da3d855b7138f94
Sergey <tel.cromax@gmail.com>
2021-09-17 14:49:29 +1100

    added junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTestJUnit5.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJUnit5.java
@@ -1,10 +1,11 @@
 package ru.netology.service;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
-public class CashbackHackServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CashbackHackServiceTestJUnit5 {
 
     @Test
     public void shouldBeRemainWithAmountUpTo1000() {
